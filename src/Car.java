9babf4a4edcbaac843030c9d2fa256fe036141bf
name3 <eunglyzhia@gmail.com>
2024-01-10 17:31:10 +0700

    first commit

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car extends Vehicle{
+    private int numberOfSeats;
+    public Car(String manufacturer, String model, int year, double baseRentalPrice, int numberOfSeats) {
+        super(manufacturer, model, year, baseRentalPrice);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public int getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
